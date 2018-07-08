f85a66e88c5d86610d8bbb789cb45afb8a319a1a
Nate Rogers <n8rogers@outlook.com>
2018-07-08 11:02:46 -0400

    adding beep for close

--- door_switch.py
+++ door_switch.py
@@ -30,6 +30,7 @@ if __name__ == '__main__':
     try:
         while True:
             if GPIO.input(SENSOR):
+                buzz.beep_count(1, 0.5)
                 door_closed()
             else:
                 buzz.beep_count(3, 0.1)
