964a90f11d920d68984be58ed9f300441e6afb1b
Nate Rogers <n8rogers@outlook.com>
2018-07-08 10:19:54 -0400

    adding buzzer class

---
+++ buzzer.py
@@ -0,0 +1,28 @@
+import time
+
+class Buzzer:
+
+    def __init__(self, GPIO, pin):
+        global GPIO = GPIO
+        global pin = pin
+        GPIO.setup(pin, GPIO.OUT)
+        GPIO.output(pin, GPIO.LOW)
+
+    def __buzz_on():
+        GPIO.output(pin, GPIO.HIGH)
+
+    def __buzz_off():
+        GPIO.output(pin, GPIO.LOW)
+
+    def beep(x):
+        __buzz_on()
+        time.sleep(x)
+        __buzz_off()
+        time.sleep(x)
+    
+    def beep_count(self, count, duration):
+        while count > 0:
+            beep(duration)
+            count = count - 1
+
+    
